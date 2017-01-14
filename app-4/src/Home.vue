e0c0a6703759c5d438b41f5a5e6a2fdb7aff47f9
guoooofu <m13224597763@163.com>
2017-01-14 19:03:22 +0800

    add a vue-router demo

---
+++ app-4/src/Home.vue
@@ -0,0 +1,16 @@
+/**
+ * @authors guoooofu (m13224597763@163.com)
+ * @date    2017-01-14 04:08:29
+ */
+ 
+<template>
+	<h1>Home Page</h1>
+</template>
+
+<script>
+	
+</script>
+
+<style scope>
+	
+</style>
---
+++ app-4/src/Users.vue
@@ -0,0 +1,34 @@
+/**
+ * @authors guoooofu (m13224597763@163.com)
+ * @date    2017-01-14 04:09:29
+ */
+ 
+<template>
+	<div>
+		<h1>Users Overview</h1>
+		<p>The user ID is: {{$route.params.id}}</p>
+		<button @click="goHome">Go Home</button>
+	</div>
+</template>
+
+<script>
+	export default{
+		watch:{
+			'$route'(to,from){
+				alert(to.params.id);
+			}
+		},
+		created(){
+			// alert(this.$route.params.id);
+		},
+		methods:{
+			goHome(){
+				this.$router.push('/');
+			}		
+		}
+	}
+</script>
+
+<style scope>
+	
+</style>
